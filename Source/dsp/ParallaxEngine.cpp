51e2a71b0950be0923207145892ee60cd0119243
SimoneDeAngelis95 <simone.de.angelis.95@gmail.com>
2026-08-14 14:32:27 +0200

    WiP

--- Source/dsp/ParallaxEngine.cpp
+++ Source/dsp/ParallaxEngine.cpp
@@ -1,1 +1,41 @@
 #include "ParallaxEngine.h"
+#include <cmath>
+
+void ParallaxEngine::prepare(double samplerate)
+{
+    this->sampleRate = samplerate;
+    delayBufferSize = static_cast<int> (std::ceil(sampleRate * MAX_DELAY_MS / 1000.0)) + 1;
+    delayBuffer.setSize (2, delayBufferSize);
+    reset();
+}
+
+void ParallaxEngine::reset()
+{
+    delayBuffer.clear();
+    writePosition = 0;
+}
+
+void ParallaxEngine::setOffsetMs(float milliseconds)
+{
+    delaySamples = static_cast<int>(std::round (milliseconds * sampleRate / 1000.0));
+}
+
+void ParallaxEngine::setDelayedSide(bool side)
+{
+    delaySide = side;
+}
+
+void ParallaxEngine::setSpread(float amount)
+{
+    spreadAmount = amount;
+}
+
+void ParallaxEngine::setWow(float amount)
+{
+    wowAmount = amount;
+}
+
+void ParallaxEngine::process(juce::AudioBuffer<float>& buffer)
+{
+    
+}
--- Source/dsp/ParallaxEngine.h
+++ Source/dsp/ParallaxEngine.h
@@ -1,17 +1,30 @@
 #pragma once
-
 #include <JuceHeader.h>
+
+#define MAX_DELAY_MS 50.0
 
 class ParallaxEngine
 {
 public:
-    void prepare (double sampleRate, int maximumBlockSize);
+    void prepare(double samplerate);
     void reset();
-
-    void setOffsetMs (float milliseconds);
-    void setDelayedSide (bool delayRight);
-    void setSpread (float amount);
-    void setWow (float amount);
-
-    void process (juce::AudioBuffer<float>& buffer);
+    
+    void setOffsetMs(float milliseconds);             // given ms convert to samples
+    void setDelayedSide(bool side);
+    void setSpread(float amount);
+    void setWow(float amount);
+    
+    void process(juce::AudioBuffer<float>& buffer);
+    
+private:
+    double sampleRate = 44100.0;
+    
+    int delaySamples = 0;
+    bool delaySide = true;                          // false = left side, true = right side
+    float spreadAmount = 1.0f;
+    float wowAmount = 0.0f;
+    
+    juce::AudioBuffer<float> delayBuffer;
+    int delayBufferSize = 0;
+    int writePosition = 0;
 };
